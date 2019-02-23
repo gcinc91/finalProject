c942441e81ebc309c5c0c57ca535645777a65a44
Gabriel Cueva <gabrielcueva_10@hotmail.com>
2019-02-23 13:36:16 +0100

    Done deploy

--- server/app.js
+++ server/app.js
@@ -91,8 +91,8 @@ app.use(
 app.use(flash());
 require("./passport")(app);
 
-// const index = require("./routes/index");
-// app.use("/", index);
+ const index = require("./routes/index");
+ app.use("/", index);
 
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
